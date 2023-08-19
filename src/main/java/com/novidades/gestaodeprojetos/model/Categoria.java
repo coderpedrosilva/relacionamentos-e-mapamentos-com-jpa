b740c7f3af913878076bc718e89f2313f16f0fe2
pedro <progerios@hotmail.com>
2023-08-18 21:33:33 -0300

    Relacionamento bidirecional - OneToMany

--- src/main/java/com/novidades/gestaodeprojetos/model/Categoria.java
+++ src/main/java/com/novidades/gestaodeprojetos/model/Categoria.java
@@ -1,10 +1,15 @@
 package com.novidades.gestaodeprojetos.model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Categoria {
@@ -15,6 +20,19 @@ public class Categoria {
     private Long id;
 
     private String descricao;
+
+    @OneToMany(mappedBy = "categoria")
+    // @JsonBackReference
+    @JsonManagedReference
+    private List<Evento> eventos;
+
+    public List<Evento> getEventos() {
+        return eventos;
+    }
+
+    public void setEventos(List<Evento> eventos) {
+        this.eventos = eventos;
+    }
 
     public Long getId() {
         return id;
